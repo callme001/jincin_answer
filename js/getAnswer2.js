a511460eb6056e8e31acb5da1e3b11918bc7123a
callme001 <3375171404@qq.com>
2016-11-24 22:35:01 +0800

    更新课后习题不能拦截答题

--- js/getAnswer2.js
+++ js/getAnswer2.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	console.log(2333);
+	console.log('课后答题拦截....');
 	$("#record").children().each(function(){
 		var selected = "input[option=" + $(this).attr("stranswer")+"]";
 		$(this).find(selected).click();
